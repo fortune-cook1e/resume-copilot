4eb935479c96d2b5e56936c7574deddf62c32154
fortune_cookie <leon.gao.dev@outlook.com>
2026-02-02 15:09:24 +0100

    feat: add common component to support A4 format

---
+++ components/resume/Page/index.tsx
@@ -0,0 +1,32 @@
+import { MM_TO_PX, PAGE_SIZE_MAP } from '@/constants';
+import { cn } from '@/lib/utils';
+import { FC, ReactNode } from 'react';
+interface Props {
+  mode?: 'builder' | 'preview';
+  children: ReactNode;
+}
+
+const Page: FC<Props> = ({ mode = 'preview', children }) => {
+  return (
+    <div
+      id="page"
+      className={cn(
+        'text-text relative bg-background bg-white',
+        mode === 'builder' && 'shadow-2xl',
+      )}
+    >
+      {children}
+
+      {mode === 'builder' && (
+        <div
+          className="absolute inset-x-0 border-b border-dashed"
+          style={{
+            top: `${PAGE_SIZE_MAP.a4.height * MM_TO_PX}px`,
+          }}
+        />
+      )}
+    </div>
+  );
+};
+
+export default Page;
