c0560f3d427d5e64aaf4b135238e66e58ddb52f9
fortune_cookie <leon.gao.dev@outlook.com>
2026-02-16 14:48:46 +0100

    feat: set cookie's name

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,12 +3,30 @@ import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/db';
 import * as schema from '@/db/schema';
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 export const auth = betterAuth({
   baseURL: process.env.NEXT_PUBLIC_APP_URL ?? 'http://localhost:3000',
   database: drizzleAdapter(db, {
     provider: 'pg',
     schema,
   }),
+  advanced: {
+    cookies: {
+      session_token: {
+        name: 'better-auth.session_token',
+        attributes: {
+          secure: isProduction,
+        },
+      },
+      session_data: {
+        name: 'better-auth.session_data',
+        attributes: {
+          secure: isProduction,
+        },
+      },
+    },
+  },
   emailAndPassword: {
     enabled: true, // To enable email and password authentication
     minPasswordLength: 8,
